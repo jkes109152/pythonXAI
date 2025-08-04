1b9e9b24e56f1fc3f6ae00633f9ed6366e6d266c
jkes109152 <jkes109152@gmail.com>
2025-08-04 16:53:39 +0800

    修正猜數字遊戲邏輯及新增數據類型轉換示範

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import random
+import math
 
+user_guess = 0
+u = 1
 i = 1
 s = 1
 b = 100
@@ -7,14 +10,19 @@ a = random.randint(1, 100)
 print("這是一個猜數字遊戲！")
 print("請猜一個介於 1 到 100 之間的數字")
 while True:
-    user_guess = 1
+    user_guess = 0
     user_guess = int(input("你的猜測是："))
-    if user_guess < a:
+    if user_guess < a and user_guess > s:
         s = user_guess
         print(f"請猜一個介於{s} 到 {b} 之間的數字")
-    elif user_guess > a:
+    elif user_guess > a and user_guess < b:
         b = user_guess
         print(f"請猜一個介於 {s} 到 {b} 之間的數字")
     elif user_guess == a:
         print("恭喜你猜對了！")
+        print(f"你總共猜了 {u} 次")
         break
+    else:
+        print(f"輸入的不是數字，或不是介於 {s} 到 {b} 之間的數字，請重新輸入")
+        u -= 1
+    u += 1
