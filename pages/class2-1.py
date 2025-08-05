231976d9b760b71f67a23001c902e06569c2fefc
jkes109152 <jkes109152@gmail.com>
2025-08-05 16:01:09 +0800

    新增邏輯運算子的範例程式碼

--- pages/class2-1.py
+++ pages/class2-1.py
@@ -5,3 +5,16 @@ print(1 > 1)  # False
 print(1 < 1)  # False
 print(1 >= 1)  # True
 print(1 <= 1)  # True
+
+# # 邏輯運算子
+# # and 運算子
+print(True and True)  # True
+print(True and False)  # False
+print(False and True)  # False
+print(False and False)  # False
+
+# # or 運算子
+print(True or True)  # True
+print(True or False)  # True
+print(False or True)  # True
+print(False or False)  # False
