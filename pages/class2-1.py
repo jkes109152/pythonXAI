5160d24fcaaad2b9b64f3c26d7f0404072a6e400
jkes109152 <jkes109152@gmail.com>
2025-08-05 16:09:12 +0800

    新增邏輯運算子和優先順序的範例程式碼

--- pages/class2-1.py
+++ pages/class2-1.py
@@ -17,4 +17,19 @@ print(False and False)  # False
 print(True or True)  # True
 print(True or False)  # True
 print(False or True)  # True
-print(False or False)  # False
+print(False or False**4)  # False
+
+
+# # not 運算子
+print(not True)  # False
+print(not False)  # True
+
+# 優先順序
+# 1. ()括號
+# 2. **次方
+# 3. * / // %乘 除 取商 取餘數
+# 4. + - 加 減
+# 5. == != > < >= <= 比較運算子
+# 6. not
+# 7. and
+# 8. or
