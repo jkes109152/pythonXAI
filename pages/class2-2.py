13af4da6749643f9338847dcd4064d1a85ccfa30
jkes109152 <jkes109152@gmail.com>
2025-08-06 09:43:29 +0800

    新增數字輸入功能，允許使用者輸入範圍在0到100之間的整數並顯示輸入結果

---
+++ pages/class2-2.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+# st.number_input()可以讓使用者輸入數字，設定step=1可以讓使用者只能輸入整數
+# min_value=0可以設定座小值為0，max_value=100可以設定最大值為100
+number = st.number_input("請輸入一個數字", step=1, min_value=0, max_value=100)
+# st.markdown()可以讓網頁使用Markdown語法來顯示文字
+st.markdown(f"你輸入的數字是：{number}")
