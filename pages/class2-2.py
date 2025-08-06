2895a3b707f6cfe1c0ab07e2dd688b1924c1b658
jkes109152 <jkes109152@gmail.com>
2025-08-06 10:07:44 +0800

    新增成績輸入功能，根據輸入的成績顯示對應的等級

--- pages/class2-2.py
+++ pages/class2-2.py
@@ -5,3 +5,17 @@ import streamlit as st
 number = st.number_input("請輸入一個數字", step=1, min_value=0, max_value=100)
 # st.markdown()可以讓網頁使用Markdown語法來顯示文字
 st.markdown(f"你輸入的數字是：{number}")
+x = st.number_input("請輸入成績", step=1, min_value=0, max_value=100)
+y = "?"
+
+if x < 60:
+    y = "F"
+elif 70 > x >= 60:
+    y = "D"
+elif 80 > x >= 70:
+    y = "C"
+elif 90 > x >= 80:
+    y = "B"
+else:
+    y = "A"
+st.markdown(f"你的等級是：{y}")
